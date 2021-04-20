51be3549dc8161737497713afea42908bc145cb8
Jason Tong <jason.tong0910@outlook.com>
2021-04-20 17:51:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,7 +7,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0 
     return "Buzz"
-  else int % 3 || int % 5 == 0 
+  else int % 3 == 0 || int % 5 == 0 
     return "FizzBuzz"
   end
   
