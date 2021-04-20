862f6881c2f255baa94a2ad131ad5696a1a1fc74
Jason Tong <jason.tong0910@outlook.com>
2021-04-20 18:40:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(15)
+def fizzbuzz(int)
   
   if 15 % 5 == 0
     return "Buzz"
